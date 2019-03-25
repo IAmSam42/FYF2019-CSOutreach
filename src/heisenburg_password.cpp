2fb7e2db2a29d5ce44cc43fc4b0eaa7790f9c898
Sam Holdcroft <sam.holdcroft42@gmail.com>
2019-03-25 00:48:38 +0000

    Added support for multiple LED panel

--- src/heisenburg_password.cpp
+++ src/heisenburg_password.cpp
@@ -1,41 +1,122 @@
 #include <FastLED.h>
+#include <cstring>
+#include <stdlib.h>
 #include "char_arrays.h"
 
+
+//Circuit configuration, doesn't need to be modified
 #define LED_PIN     13
-#define NUM_LEDS    64
+#define NUM_PANELS  2
+#define LEDS_PER_PANEL 64
+#define NUM_LEDS    NUM_PANELS*LEDS_PER_PANEL
 #define COLOR_ORDER GRB
 
-#define BASE_BRIGHTNESS 48
-#define REDUCE_BRIGHTNESS 240
+//Set the base brightness of the LEDs
+#define BASE_BRIGHTNESS 32
+//How much the 'used' LEDs are reduced in brightness. Measured as a fraction
+//out of 255  - adjust to change "difficulty"
+#define REDUCE_BRIGHTNESS 255
 
+//Number of LED panels
+
+
+
+//Set the code to be displayed on the panels
+int code[NUM_PANELS] = {0, 8};
+
+//Variable to store colour of every individual LED
 CRGB leds[NUM_LEDS];
+
+//Variable to store the "mask" that encodes the desired patterns
+int mask[NUM_LEDS] = {0};
+
+
+/**
+ *  SETUP FUNCTIONS
+ */
+
+ /**
+  * Create the mask to change the LED brightness based on the current code
+  */
+void createMask()
+{
+  //For each connected panel:
+  for(int i=0; i<NUM_PANELS; i++)
+  {
+    //Switch on the code numeral:
+    switch (code[i])
+    {
+      case 0 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_0, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 1 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_1, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 2 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_2, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 3 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_3, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 4 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_4, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 5 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_5, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 6 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_6, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 7 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_7, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 8 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_8, LEDS_PER_PANEL*sizeof(int));
+                break;
+      case 9 :  std::memcpy(&mask[i*LEDS_PER_PANEL], ar_9, LEDS_PER_PANEL*sizeof(int));
+      default:  break;
+    }
+  }
+}
+
+void setup()
+{
+  delay( 3000 ); // power-up safety delay
+  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+
+  //Set the base bightness
+  FastLED.setBrightness(  BASE_BRIGHTNESS );
+
+  //Create the mask to show the code
+  createMask();
+
+
+  srand(time(NULL));
+}
+
+
+/**
+ * LOOP FUNCTIONS
+ */
 
 /**
  * Set the LED at a given position
  */
 void setLED(int pos, CRGB col)
 {
+  //Set the colour
   leds[pos] = col;
-  leds[pos].fadeLightBy(ar_0[pos] * REDUCE_BRIGHTNESS);
-}
-
-void setup()
-{
-  delay( 3000 ); // power-up safety delay
-  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-
-  //Set the base bightness
-  FastLED.setBrightness(  BASE_BRIGHTNESS );
-
-  Serial.begin(9600);
+  //Set the brightness
+  leds[pos].fadeLightBy(mask[pos] * REDUCE_BRIGHTNESS);
 }
 
 void loop()
 {
-  for(int i=0; i<NUM_LEDS; i++)
+  //Go through every LED:
+  for(int i=0; i<(NUM_LEDS); i++)
   {
+    //Set the colour to green
     setLED(i, CRGB::Green);
-
-    FastLED.show();
   }
+
+  //Update the panel
+  FastLED.show();
+
+  //Brief delay, then change the code and make a new mask
+  delay(500);
+  code[0] = rand()%10;
+  code[1] = rand()%10;
+
+  createMask();
 }
